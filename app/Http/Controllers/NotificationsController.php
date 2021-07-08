cd1f0b60e30fa27c8b59951aad976b81435948d5
MOID <moidkhan22@yahoo.com>
2021-07-08 18:51:56 +0500

    done

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -96,9 +96,50 @@ class NotificationsController extends Controller
 
         $user = User::where('cust_account_type', '0')->get();
         foreach ($user as $key => $value) {
-            // $tokens = DeviceToken::pluck('firebase_token')->toArray();
             $tokens = DeviceToken::where('user_id', $value->id)->get()->pluck('firebase_token');
             $this->sendSingleNotification($tokens, $request->title, $request->body, $request->data);
+        }
+
+        if ($request->has('image')) {
+            $file_name = $request->image->getClientOriginalName();
+            $generated_new_name = time() . '.' . $file_name;
+            $request->photo->move($upload_path, $generated_new_name);
+            $notification =   Notifications::create([
+                'title' => $request->title,
+                'subtitle' => $request->body,
+                'data' => $request->data,
+                'image' => $upload_path . $generated_new_name,
+                'user_id' => $request->user_id,
+            ]);
+            return response()->json([
+                'success' => true,
+                'data' => $notification,
+                'meesage' => 'Notification Stored',
+            ]);
+        } else {
+            $notification =     Notifications::create([
+                'title' => $request->title,
+                'subtitle' => $request->body,
+                'data' => $request->data,
+                'user_id' => $request->user_id,
+
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'data' => $notification,
+                'meesage' => 'Notification Stored',
+            ]);
+        }
+    }
+
+    public function sendNotificationToSpecificUser(Request $request)
+    {
+        $upload_path = 'uploadedImages/notifications/';
+
+        $tokens =  DeviceToken::where('user_id', $request->user_id)->get()->pluck('firebase_token');
+        if ($tokens) {
+            $tos =    $this->sendSingleNotification($tokens, $request->title, $request->body, $request->data);
             if ($request->has('image')) {
                 $file_name = $request->image->getClientOriginalName();
                 $generated_new_name = time() . '.' . $file_name;
@@ -130,44 +171,11 @@ class NotificationsController extends Controller
                     'meesage' => 'Notification Stored',
                 ]);
             }
-        }
-    }
-
-    public function sendNotificationToSpecificUser(Request $request)
-    {
-        $upload_path = 'uploadedImages/notifications/';
-
-        $tokens =  DeviceToken::where('user_id', $request->user_id)->get()->pluck('firebase_token');
-        $tos =    $this->sendSingleNotification($tokens, $request->title, $request->body, $request->data);
-        if ($request->has('image')) {
-            $file_name = $request->image->getClientOriginalName();
-            $generated_new_name = time() . '.' . $file_name;
-            $request->photo->move($upload_path, $generated_new_name);
-            $notification =   Notifications::create([
-                'title' => $request->title,
-                'subtitle' => $request->body,
-                'data' => $request->data,
-                'image' => $upload_path . $generated_new_name,
-                'user_id' => $request->user_id,
-            ]);
-            return response()->json([
-                'success' => true,
-                'data' => $notification,
-                'meesage' => 'Notification Stored',
-            ]);
         } else {
-            $notification =     Notifications::create([
-                'title' => $request->title,
-                'subtitle' => $request->body,
-                'data' => $request->data,
-                'user_id' => $request->user_id,
-
-            ]);
-
             return response()->json([
-                'success' => true,
-                'data' => $notification,
-                'meesage' => 'Notification Stored',
+                'success' => false,
+                'data' => [],
+                'meesage' => 'No FCM TOKEN AVAILABLE',
             ]);
         }
     }
@@ -203,7 +211,6 @@ class NotificationsController extends Controller
     {
 
         $SERVER_API_KEY = 'AAAAV0DCJ4I:APA91bGCijvnYnQgphS7rUTDtpfXtVqGcJxWXM8O-nAOZSp3rE9DOUDJav8-B3oaZpZgjI3Engzj_Y3wkxJ26c0X16hv8HOrceP3NVolYel8rE55IHUvVZzD8s-fqQsXasijSUVxJLHn';
-        // $firebaseToken = DeviceToken::where('user_id', Auth::user()->id)->get()->pluck('device_token');
         $data = [
             'registration_ids' => $token,
             "notification" => [
