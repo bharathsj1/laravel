83101d6407569a5e3e3a85190db0e466b373aec2
MOID <moidkhan22@yahoo.com>
2021-07-19 01:46:57 +0500

    done

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -86,7 +86,7 @@ class SubscriptionController extends Controller
                 ],
                 'default_payment_method' => $payment_methods->data[0]->id,
             ]);
-            return redirect('/checkout/1/1')->with('status', 'Profile updated!');
+            return redirect('https://google.com.pk')->with('status', 'Profile updated!');
 
 
         } else {
