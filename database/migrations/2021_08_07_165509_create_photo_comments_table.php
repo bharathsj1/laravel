1863da1cd47766c9689d3b91bbe0794f013c1188
moid1 <43816034+moid1@users.noreply.github.com>
2021-08-07 21:57:52 +0500

    done

---
+++ database/migrations/2021_08_07_165509_create_photo_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhotoCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photo_comments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('comment_id')->default(0);
+            $table->integer('photo_id')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photo_comments');
+    }
+}
